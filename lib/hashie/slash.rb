edfb46c3f2984b2c97781964acdd57995bd94a3a
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-03-30 15:49:06 -0700

    Add a recursive walk of the slash and order unimportance specs

--- lib/hashie/slash.rb
+++ lib/hashie/slash.rb
@@ -5,5 +5,28 @@ module Hashie
     def initialize( hash={} )
       merge! hash
     end
+
+    def walk( hsh, &block )
+      hsh.each do |k, v|
+        # next unless
+        block.call v.class, k, v
+      end
+    end
+
+    def eql?( other )
+      # succeed fast if we're actually equal
+      return true if super
+
+      walk other do |type, key, value|
+        case type
+        when Array
+          self[ key ].sort == value.sort
+        when Hash
+          walk value
+        else
+          self[ key ] == value
+        end
+      end
+    end
   end
 end
--- spec/hashie/slash_spec.rb
+++ spec/hashie/slash_spec.rb
@@ -2,10 +2,53 @@ require 'spec_helper'
 
 describe Hashie::Slash do
 
-  describe 'equality' do
-    subject { Hashie::Slash.new :foo => 'bar', :baz => 'woo' }
+  describe 'order unimportance' do
+
+    describe 'of top-level keys' do
+      let( :hash ){ { :foo => 'bar', :baz => 'woo' } }
+      subject { Hashie::Slash.new hash }
+
+      it { should eql( { :foo => 'bar', :baz => 'woo' } ) }
+      it { should eql( { :baz => 'woo', :foo => 'bar' } ) }
+    end
+
+    describe 'of nested keys' do
+      let( :hash ){
+        {
+          :foo => 'bar',
+          :baz => { :a => 1, :b => 2 }
+        }
+      }
+      subject { Hashie::Slash.new hash }
+
+      it { should eql(
+        {
+          :foo => 'bar',
+          :baz => { :b => 2, :a => 1 }
+        }
+      ) }
+    end
+
+    describe 'of arrays' do
+      let( :hash ){ { :foo => [ 1, 2, 3 ] } }
+      subject { Hashie::Slash.new hash }
+
+      it { should eql( { :foo => [ 1, 2, 3 ] } ) }
+      it { should eql( { :foo => [ 3, 2, 1 ] } ) }
+    end
+  end
+
+  describe 'missing keys' do
+    let( :hash ){ { :foo => 'bar', :baz => 'woo' } }
+    subject { Hashie::Slash.new hash }
+
+    it { should eql( { :foo => 'bar', :baz => 'woo', :abc => '123' } ) }
+  end
+
+  describe 'extra keys' do
+    let( :hash ){ { :foo => 'bar', :baz => 'woo', :abc => '123' } }
+    subject { Hashie::Slash.new hash }
 
     it { should eql( { :foo => 'bar', :baz => 'woo' } ) }
-    it { should eql( { :baz => 'woo', :foo => 'bar' } ) }
   end
 end
