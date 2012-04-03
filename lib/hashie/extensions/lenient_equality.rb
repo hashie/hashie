2d83bd8df865b407c2859be6f5a4a51feda668e1
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-04-03 09:27:07 -0700

    Namespace #compare to #lenient_compare

--- lib/hashie/extensions/lenient_equality.rb
+++ lib/hashie/extensions/lenient_equality.rb
@@ -39,24 +39,24 @@ module Hashie
             # succeed fast if we're actually equal
             return true if super
             # otherwise, do a deep, lenient comparison
-            compare self, other
+            lenient_compare self, other
           end
 
           protected
 
-          def compare( original, other )
+          def lenient_compare( original, other )
             return false if not original.is_a?( other.class )
 
             case original
             when Array
               original.reduce( true ) do |memo, a|
                 memo && other.reduce( false ) do |memo, b|
-                  memo || compare( a, b )
+                  memo || lenient_compare( a, b )
                 end
               end
             when ::Hash
               original.reduce( true ) do |memo, (k, v)|
-                memo && compare( v, other[ k ] )
+                memo && lenient_compare( v, other[ k ] )
               end
             else
               original == other
