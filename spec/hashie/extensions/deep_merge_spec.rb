9a655bcd4265153368ca8c1dfbcd1e5159767cfc
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2014-01-26 14:56:23 +0000

    replacing dynamic include with recursion helper method

--- spec/hashie/extensions/deep_merge_spec.rb
+++ spec/hashie/extensions/deep_merge_spec.rb
@@ -5,16 +5,17 @@ describe Hashie::Extensions::DeepMerge do
 
   subject{ DeepMergeHash }
 
-  let(:h1) { subject.new.merge(:a => "a", :b => "b", :c => { :c1 => "c1", :c2 => "c2", :c3 => { :d1 => "d1" } }) }
-  let(:h2) { { :a => 1, :c => { :c1 => 2, :c3 => { :d2 => "d2" } } } }
-  let(:expected_hash) { { :a => 1, :b => "b", :c => { :c1 => 2, :c2 => "c2", :c3 => { :d1 => "d1", :d2 => "d2" } } } }
+  let(:h1) { subject.new.merge(:a => "a", :a1 => 42, :b => "b", :c => { :c1 => "c1", :c2 => {:a => "b"}, :c3 => { :d1 => "d1" } }) }
+  let(:h2) { { :a => 1, :a1 => 1, :c => { :c1 => 2, :c2 => "c2", :c3 => { :d2 => "d2" } } } }
+  let(:expected_hash) { { :a => 1, :a1 => 1, :b => "b", :c => { :c1 => 2, :c2 => "c2", :c3 => { :d1 => "d1", :d2 => "d2" } } } }
 
-  it 'should deep merge two hashes' do
+  it 'deep merges two hashes' do
     h1.deep_merge(h2).should == expected_hash
   end
 
-  it 'should deep merge two hashes with bang method' do
+  it 'deep merges another hash in place via bang method' do
     h1.deep_merge!(h2)
     h1.should == expected_hash
   end
+
 end
