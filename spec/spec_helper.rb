9ac737b48a54abd5b0cb233a5f49252e9cb11459
gregory <greg2502@gmail.com>
2014-06-12 10:33:49 -0700

    include the file for the specs to pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,6 @@ require 'rubygems'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'hashie/version'
 require 'hashie'
 require 'rspec'
