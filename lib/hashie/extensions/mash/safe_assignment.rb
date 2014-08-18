52a44b71051ff9579448f3b9bb0a105da4919a00
Michael Herold <michael.j.herold@gmail.com>
2014-08-18 13:00:05 -0500

    Add Hashie::Extensions::Mash::SafeAssignment
    This is part 3 of 3 of the to-do list determined in #198.

---
+++ lib/hashie/extensions/mash/safe_assignment.rb
@@ -0,0 +1,13 @@
+module Hashie
+  module Extensions
+    module Mash
+      module SafeAssignment
+        def assign_property(name, value)
+          fail ArgumentError, "The property #{name} clashes with an existing method." if methods.include?(name)
+
+          self[name] = value
+        end
+      end
+    end
+  end
+end
