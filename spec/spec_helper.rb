a3208c4a11057004b18678af112c3eb375a43710
Michael Herold <michael.j.herold@gmail.com>
2014-09-06 16:21:05 -0500

    Add simple RSpec.configure block for expect syntax
    Without the configure block, [mutant][mutant] was raising an error. I'm
    interested in mutation testing our test suite, so thought this would be
    the simplest thing to add to get that working. It also enforces the
    expect syntax, which is a nice side benefit.
    
    [mutant]: https://github.com/mbj/mutant

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'pry'
 
-require 'hashie'
 require 'rspec'
+require 'hashie'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expect|
+    expect.syntax = :expect
+  end
+end
