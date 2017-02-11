7742133ffed9443dca581da597d44fb7dc0dd96d
Michael Herold <michael.j.herold@gmail.com>
2017-02-10 19:33:02 -0600

    Clean up integration specs to test for STDOUT log
    This standardizes the way we're loading the example applications for the
    integration tests so we're actually testing the behavior of the
    application in each case. They're also structured in such a way to test
    that the Hashie logger doesn't accidentally write to the STDOUT during
    the initialization process of the applications.

--- spec/integration/omniauth-oauth2/integration_spec.rb
+++ spec/integration/omniauth-oauth2/integration_spec.rb
@@ -1,74 +1,26 @@
-$LOAD_PATH.unshift File.dirname(__FILE__)
-
-ENV['RACK_ENV'] = 'test'
+ENV['RAILS_ENV'] = 'test'
 
 require 'rspec/core'
-require 'rails'
-require 'rails/all'
-require 'action_view/testing/resolvers'
-require 'some_site'
 
-module RailsApp
-  class Application < ::Rails::Application
-    config.action_dispatch.show_exceptions            = false
-    config.active_support.deprecation                 = :stderr
-    config.eager_load                                 = false
-    config.root                                       = __dir__
-    config.secret_key_base                            = 'hashieintegrationtest'
+RSpec.describe 'omniauth-oauth2 inside of rails', type: :request do
+  let(:stdout) { StringIO.new }
 
-    routes.append do
-      get '/' => 'application#index'
-    end
-
-    config.assets.paths << File.join(__dir__, 'assets/javascripts')
-    config.assets.debug = true
+  around(:each) do |example|
+    original_stdout = $stdout
+    $stdout = stdout
+    require_relative 'app'
+    require 'rspec/rails'
+    example.run
+    $stdout = original_stdout
   end
-end
 
-LAYOUT = <<-HTML
-<!DOCTYPE html>
-<html>
-<head>
-  <title>TestApp</title>
-  <%= stylesheet_link_tag    "application", :media => "all" %>
-  <%= javascript_include_tag "application" %>
-  <%= csrf_meta_tags %>
-</head>
-<body>
-<%= yield %>
-</body>
-</html>
-HTML
-
-INDEX = <<-HTML
-<h1>Hello, world!</h1>
-HTML
-
-class ApplicationController < ActionController::Base
-  include Rails.application.routes.url_helpers
-
-  layout 'application'
-
-  self.view_paths = [ActionView::FixtureResolver.new(
-    'layouts/application.html.erb'         => LAYOUT,
-    'application/index.html.erb'           => INDEX
-  )]
-
-  def index
-  end
-end
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :some_site
-end
-
-# the order is important
-# hashie must be loaded first to register the railtie
-require 'hashie'
-RailsApp::Application.initialize!
-
-RSpec.describe 'the Hashie logger' do
-  it 'is set to the Rails logger' do
+  it 'does not log anything to STDOUT when initializing a Rails app and is set to Rails logger' do
+    expect(stdout.string).to eq('')
     expect(Hashie.logger).to eq(Rails.logger)
+  end
+
+  it 'works' do
+    get '/'
+    assert_select 'h1', 'Hello, world!'
   end
 end
--- spec/integration/omniauth/integration_spec.rb
+++ spec/integration/omniauth/integration_spec.rb
@@ -2,33 +2,34 @@ ENV['RACK_ENV'] = 'test'
 
 require 'rspec/core'
 require 'rack/test'
-require 'sinatra'
-require 'omniauth'
-
-class MyApplication < Sinatra::Base
-  use Rack::Session::Cookie, secret: 'hashie integration tests'
-  use OmniAuth::Strategies::Developer
-
-  get '/' do
-    'Hello World'
-  end
-end
-
-module RSpecMixin
-  include Rack::Test::Methods
-  def app
-    MyApplication
-  end
-end
 
 RSpec.configure do |config|
-  config.include RSpecMixin
   config.expect_with :rspec do |expect|
     expect.syntax = :expect
   end
 end
 
-describe 'omniauth' do
+RSpec.describe 'omniauth' do
+  include Rack::Test::Methods
+
+  def app
+    MyApplication
+  end
+
+  let(:stdout) { StringIO.new }
+
+  around(:each) do |example|
+    original_stdout = $stdout
+    $stdout = stdout
+    require_relative 'app'
+    example.run
+    $stdout = original_stdout
+  end
+
+  it 'does not log anything to STDOUT when initializing' do
+    expect(stdout.string).to eq('')
+  end
+
   it 'works' do
     get '/'
     expect(last_response).to be_ok
