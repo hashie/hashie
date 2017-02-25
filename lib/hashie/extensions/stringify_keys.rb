2e9f99936d414b91d4bab660047d30cd82eb003f
Michael Herold <michael.j.herold@gmail.com>
2017-02-25 11:42:21 -0600

    Improve docs on two extensions

--- lib/hashie/extensions/stringify_keys.rb
+++ lib/hashie/extensions/stringify_keys.rb
@@ -1,27 +1,64 @@
 module Hashie
   module Extensions
+    # Extends a Hash to have the ability to convert its keys to strings
+    #
+    # @example Extend a pre-existing Hash object
+    #   hash = { test: 'value' }
+    #   hash.extend(Hashie::Extensions::StringifyKeys)
+    #   hash.stringify_keys!
+    #   hash  #=> { 'test' => 'value' }
+    #
+    # @example Create a Hash class with this capability
+    #   class StringifyingHash < Hash
+    #     include Hashie::Extensions::StringifyKeys
+    #   end
+    #
+    #   hash = StringifyingHash.new
+    #   hash[:test] = 'value'
+    #   hash.stringify_keys!
+    #   hash  #=> { 'test' => 'value' }
+    #
+    # @example Use the module function to stringify keys in a hash
+    #   hash = { test: 'value' }
+    #   Hashie::Extensions::StringifyKeys.stringify_keys!(hash)
+    #   hash  #=> { 'test' => 'value' }
     module StringifyKeys
-      # Convert all keys in the hash to strings.
+      # Converts all keys in the hash to strings
       #
       # @example
-      #   test = {:abc => 'def'}
+      #   test = { abc: 'def' }
+      #   text.extend(Hashie::Extensions::StringifyKeys)
       #   test.stringify_keys!
-      #   test # => {'abc' => 'def'}
+      #   test # => { 'abc' => 'def' }
+      #
+      # @api public
+      # @return [Hash] the hash with its keys converted to strings
       def stringify_keys!
         StringifyKeys.stringify_keys!(self)
         self
       end
 
-      # Return a new hash with all keys converted
-      # to strings.
+      # Returns a new hash with all keys converted to strings
+      #
+      # @example
+      #   test = { abc: 'def' }
+      #   text.extend(Hashie::Extensions::StringifyKeys)
+      #   test.stringify_keys  #=> { 'abc' => 'def' }
+      #   test  #=> { abc: 'def' }
+      #
+      # @api public
+      # @return [Hash] a copy of the hash with its keys converted to strings
       def stringify_keys
         StringifyKeys.stringify_keys(self)
       end
 
+      # The methods that will be available on the module itself
       module ClassMethods
-        # Stringify all keys recursively within nested
-        # hashes and arrays.
+        # Stringifies all keys recursively within nested hashes and arrays
+        #
         # @api private
+        # @param [Object] object
+        # @return [Hash] the object with all of its hashes converted to strings
         def stringify_keys_recursively!(object)
           case object
           when self.class
@@ -35,13 +72,16 @@ module Hashie
           end
         end
 
-        # Convert all keys in the hash to strings.
+        # Converts all keys in a Hash to strings
         #
-        # @param [::Hash] hash
         # @example
-        #   test = {:abc => 'def'}
-        #   test.stringify_keys!
+        #   test = {abc: 'def'}
+        #   Hashie.stringify_keys!(test)
         #   test # => {'abc' => 'def'}
+        #
+        # @api public
+        # @param [Hash] hash
+        # @return [Hash] the passed Hash with its keys as strings
         def stringify_keys!(hash)
           hash.extend(Hashie::Extensions::StringifyKeys) unless hash.respond_to?(:stringify_keys!)
           hash.keys.each do |k|
@@ -51,9 +91,16 @@ module Hashie
           hash
         end
 
-        # Return a copy of hash with all keys converted
-        # to strings.
+        # Returns a copy of a Hash with all keys converted to strings
+        #
+        # @example
+        #   test = { abc: 'def' }
+        #   Hashie.stringify_keys(test)  #=> { 'abc' => 'def' }
+        #   test  #=> { abc: 'def' }
+        #
+        # @api public
         # @param [::Hash] hash
+        # @return [Hash] a copy of the Hash with stringified keys
         def stringify_keys(hash)
           copy = hash.dup
           copy.extend(Hashie::Extensions::StringifyKeys) unless copy.respond_to?(:stringify_keys!)
