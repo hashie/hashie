a59d200849b1f579f4ff4369816ea7aeba295fd6
Michael Herold <opensource@michaeljherold.com>
2018-08-14 21:33:09 -0500

    Fix a regression with aliases on `Mash.load`
    Also, reorganize the test into the existing file and update the
    changelog.

--- spec/hashie/mash_is_loading_yaml_spec.rb
+++
@@ -1,8 +0,0 @@
-require "spec_helper"
-
-context "load yaml" do
-  it do
-    mash = Hashie::Mash.load("spec/fixtures/yaml_with_aliases.yml")
-    expect(mash.company_a.accounts.admin.password).to eq "secret"
-  end
-end
