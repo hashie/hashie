12ae69adf3ec280576fd55d5f0c6a6d0cc107567
Alexander Graul <agraul@suse.com>
2018-09-22 21:07:33 +0200

    Remove superfluous comment
    After the last commit, the code is self-explanatory.
    
    Co-authored-by: Ana María Martínez Gómez <ammartinez@suse.de>

--- lib/hashie/extensions/coercion.rb
+++ lib/hashie/extensions/coercion.rb
@@ -162,7 +162,7 @@ module Hashie
             if type.class == ::Hash
               type, key_type, value_type = type.class, *type.first
               build_hash_coercion(type, key_type, value_type)
-            else # Enumerable but not Hash: Array, Set
+            else
               value_type = type.first
               type = type.class
               build_container_coercion(type, value_type)
