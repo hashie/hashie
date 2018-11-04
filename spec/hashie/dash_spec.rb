1c767b7fd6fdca80ec0bdf676d2575cb2c0e48d9
Harry Lascelles <harry@harrylascelles.com>
2018-11-04 17:11:07 +0000

    Add a failing test that demonstrates that nil valued keys are no longer being written out

--- spec/hashie/dash_spec.rb
+++ spec/hashie/dash_spec.rb
@@ -438,6 +438,16 @@ describe DashTest do
       end
     end
   end
+
+  describe '#to_h' do
+    it 'writes out keys with nil values' do
+      expect(DashTest.new(first_name: 'Bob', email: nil).to_h).to eq(
+        first_name: "Bob",
+        email: nil,
+        count: 0,
+      )
+    end
+  end
 end
 
 describe Hashie::Dash, 'inheritance' do
