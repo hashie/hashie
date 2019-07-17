51248053189572b63ae3e5843bbe6a0982603179
Bobby McDonald <bobbymcwho@gmail.com>
2019-07-17 13:05:39 -0400

    Railtie should work with Rails != 6

--- lib/hashie/railties/mash.rb
+++ lib/hashie/railties/mash.rb
@@ -3,7 +3,7 @@ module Hashie
   class Mash < Hash
     def except(*keys)
       string_keys = keys.map { |key| convert_key(key) }
-      slice(*self.keys - string_keys)
+      super(*string_keys)
     end
   end
 end
