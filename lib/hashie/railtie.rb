c4dd753b4c6fa5d9aa95cc89c49876886fa4f15a
Bobby McDonald <bobbymcwho@gmail.com>
2019-07-17 13:15:51 -0400

    Always require the railtie after active_support has been loaded

--- lib/hashie/railtie.rb
+++ lib/hashie/railtie.rb
@@ -1,12 +1,15 @@
 begin
   require 'rails/railtie'
-  require_relative 'railties/mash'
 
   module Hashie
     class Railtie < Rails::Railtie
       # Set the Hashie.logger to use Rails.logger when used with rails.
       initializer 'hashie.configure_logger', after: 'initialize_logger' do
         Hashie.logger = Rails.logger
+      end
+
+      initializer 'hashie.patch_hash_except', after: 'load_active_support' do
+        require 'hashie/railties/mash'
       end
     end
   end
