6114fb6d1676a0de5d3fdfbb9e7b4ca50251e86a
vmarutha <vinoth.maruthalingam@gmail.com>
2020-07-25 12:35:07 -0400

    rubocop fix

--- lib/hashie/extensions/grep.rb
+++ lib/hashie/extensions/grep.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'hashie/extensions/deep_locate'
 module Hashie
   module Extensions
@@ -32,15 +34,15 @@ module Hashie
       private
 
       def _match_text?(pattern, text)
-        return false unless (text.instance_of?(String) || text.instance_of?(Symbol))
+        return false unless text.instance_of?(String) || text.instance_of?(Symbol)
 
         !pattern.match(text).nil?
       end
 
       def _grep(pattern, object = self, matches = [])
-        grep_result = DeepLocate.deep_locate -> (key, value, object) { 
+        grep_result = DeepLocate.deep_locate(lambda do |key, value, _object|
           _match_text?(pattern, key) || _match_text?(pattern, value)
-        }, object
+        end, object)
 
         matches.concat(grep_result)
       end
