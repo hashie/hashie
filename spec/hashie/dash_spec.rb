ac2444c3c9d020b2d0ef1b011b5bdb37d2b4af62
Michael Herold <opensource@michaeljherold.com>
2020-10-22 20:51:14 -0500

    Fix an imprecise spec for `Dash#replace`
    The check here doesn't make sense because it's testing the behavior of
    another method. The best way to check identity is but checking the
    `#object_id` of an object - thus, this check actually does what the test
    says it does.

--- spec/hashie/dash_spec.rb
+++ spec/hashie/dash_spec.rb
@@ -344,7 +344,7 @@ describe DashTest do
     before { subject.replace(first_name: 'Cain') }
 
     it 'return self' do
-      expect(subject.replace(email: 'bar').to_hash).to eq(email: 'bar', count: 0)
+      expect(subject.replace(email: 'bar').object_id).to eq subject.object_id
     end
 
     it 'sets all specified keys to their corresponding values' do
