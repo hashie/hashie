c15bd49f130757b1039e2f3a7dcaa93fdfd47755
Abram Flansburg <abeflansburg@gmail.com>
2022-11-04 10:32:34 -0500

    new integration spec for rails 7 exhibiting failure when executing deep_<op>_keys on Hashie::Dash

---
+++ spec/integration/rails_7/app.rb
@@ -0,0 +1,40 @@
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_view/testing/resolvers'
+require 'rails/test_unit/railtie'
+
+module RailsApp
+  class Application < ::Rails::Application
+    config.eager_load      = false
+    config.secret_key_base = 'hashieintegrationtest'
+
+    routes.append do
+      get '/' => 'application#index'
+    end
+  end
+end
+
+PAGE = <<-HTML.freeze
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TestApp</title>
+  <%= csrf_meta_tags %>
+</head>
+<body>
+  <h1>Hello, world!</h1>
+</body>
+</html>
+HTML
+
+class ApplicationController < ActionController::Base
+  include Rails.application.routes.url_helpers
+
+  def index
+    render inline: PAGE
+  end
+end
+
+Bundler.require(:default, Rails.env)
+
+RailsApp::Application.initialize!
