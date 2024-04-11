4865d5c797beac5711058d6c5eb97f2dbf9f0841
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2024-04-11 10:07:09 -0400

    Fix: integration tests in CI.

--- spec/integration/omniauth/app.rb
+++ spec/integration/omniauth/app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'omniauth'
+require 'securerandom'
 
 class MyApplication < Sinatra::Base
-  use Rack::Session::Cookie, secret: 'hashie integration tests'
+  use Rack::Session::Cookie, secret: SecureRandom.hex(64)
   use OmniAuth::Strategies::Developer
 
   get '/' do
